52ac36873e333fcf86540842ad0ee3e126922dac
Gastón Cuesta <gastoncuesta92@gmail.com>
2022-05-11 18:40:19 +0200

    Refactor test

--- src/test/java/TriangleTypeApplication.java
+++ src/test/java/TriangleTypeApplication.java
@@ -1,6 +1,7 @@
 import model.Triangle;
 import org.junit.jupiter.api.Test;
 
+import static model.Triangle.TriangleType.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -10,21 +11,21 @@ public class TriangleTypeApplication {
     void testIsEquilateral() {
         Triangle triangle = new Triangle(5, 5, 5);
         assertTrue(triangle.isEquilateral());
-        assertEquals("EQUILATERAL", triangle.getType().name());
+        assertEquals(EQUILATERAL, triangle.getType());
     }
 
     @Test
     void testIsIsosceles() {
         Triangle triangle = new Triangle(5, 2, 5);
         assertTrue(triangle.isIsosceles());
-        assertEquals("ISOSCELES", triangle.getType().name());
+        assertEquals(ISOSCELES, triangle.getType());
     }
 
     @Test
     void testIsScalene() {
         Triangle triangle = new Triangle(4, 2, 5);
         assertTrue(triangle.isScalene());
-        assertEquals("SCALENE", triangle.getType().name());
+        assertEquals(SCALENE, triangle.getType());
     }
 
     @Test
